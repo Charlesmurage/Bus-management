569fd8950920d883555911fcad249f64826c8152
Charles maina <muragemaina09@gmail.com>
2020-02-06 08:42:43 +0300

    fixed errors

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/charles/Documents/projects/Bus-reservation-system-in-python-django-2.1/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

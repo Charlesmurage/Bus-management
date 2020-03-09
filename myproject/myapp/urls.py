723489cd201242b4a0e435739893d559799feda6
Charles maina <muragemaina09@gmail.com>
2020-03-09 13:40:58 +0300

    changed path to urls

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('findbus', views.findbus, name="findbus"),
-    path('bookings', views.bookings, name="bookings"),
-    path('cancellings', views.cancellings, name="cancellings"),
-    path('seebookings', views.seebookings, name="seebookings"),
-    path('signup', views.signup, name="signup"),
-    path('signin', views.signin, name="signin"),
-    path('success', views.success, name="success"),
-    path('signout', views.signout, name="signout"),
+    url(r'^$', views.home, name="home"),
+    url(r'^findbus/$', views.findbus, name="findbus"),
+    url(r'^bookings/$', views.bookings, name="bookings"),
+    url(r'^cancellings/$', views.cancellings, name="cancellings"),
+    url(r'^seebookings/$', views.seebookings, name="seebookings"),
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'^signin/$', views.signin, name="signin"),
+    url(r'success/$', views.success, name="success"),
+    url('signout', views.signout, name="signout"),
 
 ]
